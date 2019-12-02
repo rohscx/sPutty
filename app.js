ffb78cdd01cac907ec9b62e4fe2b73a8f5face45
Foster <Erou.Foster@financialpartners.com>
2019-12-02 12:08:32 -0500

    first push

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const powerShellPutty = require('./lib/powerShellPutty.js');
 //Main App
+const platform = process.platform; //?
+const args = process.argv;
+const uName = args[2];
+const uPassword =  args[3];
+if (!uName || !uPassword) return console.log("Missing arugment: userName and userPassord required")
+const winSputty = new powerShellPutty(uName,uPassword)
+return winSputty.sPutty() //?
