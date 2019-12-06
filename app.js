117000f66c82c0f6b268222f1debabb9d9e1b539
Foster <Erou.Foster@financialpartners.com>
2019-12-06 15:56:52 -0500

    update

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ const uName = args[2];
 const uPassword =  args[3];
 if (!uName || !uPassword) return console.log("Missing arugment: userName and userPassord required")
 const winSputty = new powerShellPutty(uName,uPassword)
-return winSputty.sPutty() //?
+winSputty.sPutty() //?
